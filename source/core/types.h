fc814bb4de6e6c1525c5ec8bea38612af7c3cdb7
ycwn <ycwn@poseidon>
2017-09-03 01:25:04 +0300

    ~ Implementation of helper functions

--- source/core/types.h
+++ source/core/types.h
@@ -20,12 +20,29 @@ typedef double  f64;
 typedef unsigned int  uint;
 
 
-typedef f32 V2[2];
-typedef f32 V3[3];
-typedef f32 V4[4];
+typedef float V2[2];
+typedef float V3[3];
+typedef float V4[4];
 
-typedef f32 M3[9];
-typedef f32 M4[16];
+typedef float M3[9];
+typedef float M4[16];
+
+
+static inline int   mini(int   x, int   y) { return (x < y)? x: y; }
+static inline uint  minu(uint  x, uint  y) { return (x < y)? x: y; }
+static inline float minf(float x, float y) { return (x < y)? x: y; }
+
+static inline int   maxi(int   x, int   y) { return (x > y)? x: y; }
+static inline uint  maxu(uint  x, uint  y) { return (x > y)? x: y; }
+static inline float maxf(float x, float y) { return (x > y)? x: y; }
+
+static inline int   clampi(int   x, int   min, int   max) { return mini(maxi(x, min), max); }
+static inline uint  clampu(uint  x, uint  min, uint  max) { return minu(maxu(x, min), max); }
+static inline float clampf(float x, float min, float max) { return minf(maxf(x, min), max); }
+
+
+#define countof(x) (sizeof(x) / sizeof((x)[0]))
+#define mzero(x)   memset((x), 0, sizeof(x))
 
 
 #endif
